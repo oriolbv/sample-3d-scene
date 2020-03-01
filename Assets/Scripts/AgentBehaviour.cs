62d019edc1ac6677ef22998b972113193ee9ae1d
Oriol Burgaya <oriolbv@gmail.com>
2020-03-01 19:09:48 +0100

    Agent behaviour implemented to follow mouse pointer

---
+++ Assets/Scripts/AgentBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(camRay, out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
