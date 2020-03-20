74eb7d73d28b04bb75ffb5493dc53a043beca548
Oriol Burgaya <oriolbv@gmail.com>
2020-03-20 20:14:58 +0100

    FSMPursuer interface added

---
+++ Assets/Scripts/FSMPursuer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FSMPursuer : MonoBehaviour
+{
+    // List of the different states
+    [HideInInspector] public IStatePursuer stateActual;
+    [HideInInspector] public StatePursuePlayer statePursuePlayer;
+    [HideInInspector] public StatePursueEnemy statePursueEnemy;
+
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        statePursuePlayer = new StatePursuePlayer(this);
+        statePursueEnemy = new StatePursueEnemy(this);
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
+ 
+public interface IStatePursuer
+{
+    void UpdateState();
+
+    void ToStatePursuePlayer();
+
+    void ToStatePursueEnemy();
+}
+
+public class StatePursuePlayer : IStatePursuer
+{
+    private readonly FSMPursuer fsm;
+    public StatePursuePlayer(FSMPursuer fsmSoldado)
+    {
+        fsm = fsmSoldado;
+    }
+
+    public void ToStatePursueEnemy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ToStatePursuePlayer()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class StatePursueEnemy : IStatePursuer
+{
+    private readonly FSMPursuer fsm;
+    public StatePursueEnemy(FSMPursuer fsmSoldado)
+    {
+        fsm = fsmSoldado;
+    }
+    public void ToStatePursueEnemy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ToStatePursuePlayer()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
